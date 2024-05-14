08f166497e498ba4af0f8b3a44aaa9d25037b79b
Jamin Yang <jamin.yang@mail.utoronto.ca>
2024-05-14 13:59:20 -0400

    Shopping Cart HTML

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const priceText = document.querySelector('#total');
+const purchaseInfo = []
+
+// const buttons = document.querySelectorAll('.purchase')
+// buttons.forEach(btn => {
+//     purchaseInfo.push()
+//     btn.addEventListener('click', ()=>{
+//         priceAdjust(btn);
+//     });
+// }); 
+
+
+// function priceAdjust(btn){
+//     let total = parseFloat(priceText.textContent);
+//     // console.log(btn)
+//     // const btnText = ;
+//     console.log(total)
+//     console.log(btn.textContent)
+//     total += parseFloat(btn.textContent.replace("$", ""));
+//     priceText.textContent = total;
+// }
+const items = document.querySelectorAll('.grid-item')
+console.log(items[0].children)
+// buttons.forEach(btn => {
+//     purchaseInfo.push()
+//     btn.addEventListener('click', ()=>{
+//         priceAdjust(btn);
+//     });
+// }); 
+
+
+// function priceAdjust(btn){
+//     let total = parseFloat(priceText.textContent);
+//     // console.log(btn)
+//     // const btnText = ;
+//     console.log(total)
+//     console.log(btn.textContent)
+//     total += parseFloat(btn.textContent.replace("$", ""));
+//     priceText.textContent = total;
+// }
